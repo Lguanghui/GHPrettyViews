58e06c76996d89bb97ead714226e0922eabcbe66
liangguanghui <liangguanghui@keep.com>
2022-02-12 21:31:37 +0800

    update package

--- Package.swift
+++ Package.swift
@@ -20,8 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GHPrettyViews",
-            dependencies: [],
-        exclude: ["../../ExampleProj/"]),
+            dependencies: []),
         .testTarget(
             name: "GHPrettyViewsTests",
             dependencies: ["GHPrettyViews"]),
