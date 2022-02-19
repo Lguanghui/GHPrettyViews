1b095e8578989266588e16195c8a976418c6dd25
liangguanghui <liangguanghui@keep.com>
2022-02-20 00:20:52 +0800

    add xcworkspace

--- ExampleProj/ExampleProj/ViewController.swift
+++ ExampleProj/ExampleProj/ViewController.swift
@@ -6,15 +6,29 @@
 //
 
 import UIKit
-import GHPrettyViews
+
+import GHFoundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-    let test = GHPrettyViews()
+    
+    private let tableView = UITableView().then { table in
+        table.showsVerticalScrollIndicator = false
+    }
+    
+    private let controllersDic: [String: AnyClass] = [:]
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return controllersDic.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
---
+++ ExampleProj/ExampleProj/ViewControllers/AlbumShadowView.swift
@@ -0,0 +1,17 @@
+//
+//  AlbumShadowView.swift
+//  ExampleProj
+//
+//  Created by 梁光辉 on 2022/2/12.
+//
+
+import UIKit
+
+class AlbumShadowView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
